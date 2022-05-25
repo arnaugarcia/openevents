eb6e46b628a116041d5f69b2d3a787dd423d979c
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-05-25 19:07:12 +0200

    Implemented users update method

--- modules/users/users.route.js
+++ modules/users/users.route.js
@@ -12,8 +12,8 @@ router.get('/:id', async (req, res, next) => {
     res.json(await userService.find(req.params.id));
 })
 
-router.put('/:id', (req, res, next) => {
-    next("not implemented yet")
+router.put('/:id', async (req, res, next) => {
+    res.json(await userService.update(req.params.id, req.body));
 })
 
 router.delete('/:id', (req, res, next) => {
--- modules/users/users.service.js
+++ modules/users/users.service.js
@@ -9,6 +9,12 @@ class UsersService extends GenericCrudService {
         const results = await global.connection.promise().query("SELECT * FROM users WHERE email = ?", [email]);
         return results[0][0];
     }
+
+    async update(id, data) {
+        console.log(data);
+        const results = await global.connection.promise().query(`UPDATE users t SET t.name = '${data.name}', t.last_name = '${data.last_name}', t.email = '${data.email}', t.image = '${data.image}' WHERE t.id = ${id}`, [data]);
+        return results[0][0];
+    }
 }
 
 module.exports = UsersService;
