dc960c5b0d4d3b333d5f35128c463bb70c87aefc
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-05-25 18:40:53 +0200

    Implemented find by id on users endpoint

--- modules/users/users.route.js
+++ modules/users/users.route.js
@@ -8,8 +8,8 @@ router.get('/', async (req, res, next) => {
     res.json(await userService.findAll());
 })
 
-router.get('/:id', (req, res, next) => {
-    next("not implemented yet")
+router.get('/:id', async (req, res, next) => {
+    res.json(await userService.find(req.params.id));
 })
 
 router.put('/:id', (req, res, next) => {
