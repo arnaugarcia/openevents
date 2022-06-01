5deb7e9cafa6f2f732d45d462870ac866d01dcfa
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-06-01 17:43:08 +0200

    Implemented update events route and service

--- modules/events/events.route.js
+++ modules/events/events.route.js
@@ -12,8 +12,16 @@ router.post('/', async (req, res, next) => {
     res.json(await eventsService.saveForUser(req.USER_ID, req.body));
 })
 
+router.get('/search', async (req, res, next) => {
+    res.json(await eventsService.search(req.query));
+})
+
 router.get('/:id', async (req, res, next) => {
     res.json(await eventsService.find(req.params.id));
+})
+
+router.put('/:id', async (req, res, next) => {
+    res.json(await eventsService.update(req.params.id, req.body));
 })
 
 module.exports = router;
