92e4ebc25ff4f9828b6011febc2180b8d09a6e80
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-06-01 17:05:56 +0200

    Implemented find by id method for events

--- modules/events/events.route.js
+++ modules/events/events.route.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const HttpStatus = require('http-status-codes');
-
 const EventsService = require('./events.service');
 const eventsService = new EventsService();
 
@@ -12,6 +10,10 @@ router.get('/', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
     res.json(await eventsService.saveForUser(req.USER_ID, req.body));
+})
+
+router.get('/:id', async (req, res, next) => {
+    res.json(await eventsService.find(req.params.id));
 })
 
 module.exports = router;
