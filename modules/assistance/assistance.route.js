68f0435edfb2d17d22c6e7b25c350612c95c5b59
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-06-04 17:41:11 +0200

    Created endpoint to find assistance for the desired user and event

--- modules/assistance/assistance.route.js
+++ modules/assistance/assistance.route.js
@@ -6,8 +6,8 @@ const HttpStatus = require('http-status-codes');
 const AssistanceService = require('./assistance.service');
 const assistanceService = new AssistanceService();
 
-router.get('/', async (req, res, next) => {
-    res.json(await assistanceService.findAll());
+router.get('/:userId/:eventId', async (req, res, next) => {
+    res.json(await assistanceService.findAssistancesForEventAndUser(req.params.eventId, req.params.userId));
 })
 
 module.exports = router;
